a05e6625921bb7413804cb16857f33031e812cc9
Victor Leonardo Cordero Espinoza <vcorderoespinoza@gmail.com>
2018-07-16 17:39:47 -0500

    refactoring names

--- CleanCode/01 Names/Names.cs
+++ CleanCode/01 Names/Names.cs
@@ -2,16 +2,16 @@
 
 namespace CleanCode.Names
 {
-    public class Names
+    public class BitmapDrawer 
     {
-        public Bitmap Method1(string n)
+        public Bitmap DrawBitmap (string fileName)
         {
-            var b = new Bitmap(n);
-            var g = Graphics.FromImage(b);
-            g.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
-            g.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
-            g.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
-            return b;
+            var bitmapImage = new Bitmap(fileName);
+            var bitmapGraphic = Graphics.FromImage(bitmapImage);
+            bitmapGraphic.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
+            bitmapGraphic.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
+            bitmapGraphic.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
+            return bitmapImage;
         }
     }
 }
