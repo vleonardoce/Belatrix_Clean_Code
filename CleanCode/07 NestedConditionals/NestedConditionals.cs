5c77b7070d3de4deac5eddc3508da7f86501d14d
Victor Leonardo Cordero Espinoza <vcorderoespinoza@gmail.com>
2018-07-18 18:43:50 -0500

    JP - Victor Refactor nested conditionals

--- CleanCode/07 NestedConditionals/NestedConditionals.cs
+++ CleanCode/07 NestedConditionals/NestedConditionals.cs
@@ -19,36 +19,33 @@ namespace CleanCode.NestedConditionals
         public Customer Customer { get; set; }
         public bool IsCanceled { get; set; }
 
+        public enum ClientTypesHours : int
+        {
+            Gold = 24,
+            Regular = 48
+        }
+
         public void Cancel()
         {
             // Gold customers can cancel up to 24 hours before
             if (Customer.LoyaltyPoints > 100)
             {
-                // If reservation already started throw exception
-                if (DateTime.Now > From)
-                {
-                    throw new InvalidOperationException("It's too late to cancel.");
-                }
-                if ((From - DateTime.Now).TotalHours < 24)
-                {
-                    throw new InvalidOperationException("It's too late to cancel.");
-                }
-                IsCanceled = true;
+                ValidateHours(From, ClientTypesHours.Gold);
             }
             else
             {
                 // Regular customers can cancel up to 48 hours before
+                ValidateHours(From, ClientTypesHours.Regular);
+            }
+            IsCanceled = true;
+        }
 
-                // If reservation already started throw exception
-                if (DateTime.Now > From)
-                {
-                    throw new InvalidOperationException("It's too late to cancel.");
-                }
-                if ((From - DateTime.Now).TotalHours < 48)
-                {
-                    throw new InvalidOperationException("It's too late to cancel.");
-                }
-                IsCanceled = true;
+        public void ValidateHours(DateTime From, ClientTypesHours Hours)
+        {
+            // If reservation already started throw exception
+            if (DateTime.Now > From || (From - DateTime.Now).TotalHours < (int)Hours)
+            {
+                throw new InvalidOperationException("It's too late to cancel.");
             }
         }
 
