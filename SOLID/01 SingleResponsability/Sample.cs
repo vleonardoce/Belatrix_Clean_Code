e84e0d66930b1ada33b492111b547b8cc9100d6d
Victor Leonardo Cordero Espinoza <vcorderoespinoza@gmail.com>
2018-07-24 12:07:25 -0500

    refactor SOLID single responsibility sample

--- SOLID/01 SingleResponsability/Sample.cs
+++ SOLID/01 SingleResponsability/Sample.cs
@@ -4,14 +4,20 @@ namespace SOLID._01_SingleResponsability
 {
     public class Sample
     {
-        public void Sum(int a, int b)
+        public void Sum(int number1, int number2)
         {
-            var c = a + b;
-            var m = "";
-            if (c > 0 && c <= 10) m = "Value between 0 and 10";
-            else if (c > 10 && c <= 20) m = "Value between 11 and 20";
-            else if (c > 20 && c <= 30) m = "Value between 11 and 20";
-            Console.WriteLine(string.Format("The sum is: {0} and is in range {1}", c, m));
+            var total = number1 + number2;
+            var rangeDescription = GetRangeDescription(total);
+            Console.WriteLine(string.Format("The sum is: {0} and is in range {1}", total, rangeDescription));
+        }
+
+        private static string GetRangeDescription(int total)
+        {
+            var rangeDescription = "";
+            if (total > 0 && total <= 10) rangeDescription = "Value between 0 and 10";
+            else if (total > 10 && total <= 20) rangeDescription = "Value between 11 and 20";
+            else if (total > 20 && total <= 30) rangeDescription = "Value between 11 and 20";
+            return rangeDescription;
         }
     }
 }
