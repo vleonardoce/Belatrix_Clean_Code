8c2007f8bf7072da95e114521bd3f604410c3ef2
Victor Leonardo Cordero Espinoza <vcorderoespinoza@gmail.com>
2018-07-26 09:22:31 -0500

    refactor SOLID interface segregation sample

--- SOLID/04 Interface Segregation/IPrintTasks.cs
+++ SOLID/04 Interface Segregation/IPrintTasks.cs
@@ -2,58 +2,41 @@
 
 namespace SOLID._04_Interface_Segregation
 {
-    public interface IPrintTasks
+    public abstract class MultifunctionalPrinter
     {
-        bool PrintContent(string content);
-        bool ScanContent(string content);
-        bool FaxContent(string content);
-        bool PhotoCopyContent(string content);
-        bool PrintDuplexContent(string content);
-    }
-
-    public class HPLaserJet : IPrintTasks
-    {
-        public bool FaxContent(string content)
+        public virtual bool PrintContent(string content)
         {
             Console.WriteLine("Fax Done"); return true;
         }
-        public bool PhotoCopyContent(string content)
+        public virtual bool ScanContent(string content)
         {
-            Console.WriteLine("PhotoCopy Done"); return true;
+            Console.WriteLine("Fax Done"); return true;
         }
-        public bool PrintContent(string content)
+        public virtual bool FaxContent(string content)
         {
-            Console.WriteLine("Print Done"); return true;
+            Console.WriteLine("Fax Done"); return true;
         }
-        public bool PrintDuplexContent(string content)
+        public virtual bool PhotoCopyContent(string content)
         {
-            Console.WriteLine("Print Duplex Done"); return true;
+            Console.WriteLine("Fax Done"); return true;
         }
-        public bool ScanContent(string content)
+        public virtual bool PrintDuplexContent(string content)
         {
-            Console.WriteLine("Scan Done"); return true;
+            Console.WriteLine("Fax Done"); return true;
         }
     }
 
-    public class CannonMG2470 : IPrintTasks
+    public class HPLaserJet : MultifunctionalPrinter
     {
-        public bool PhotoCopyContent(string content)
-        {
-            Console.WriteLine("PhotoCopy Done"); return true;
-        }
-        public bool PrintContent(string content)
-        {
-            Console.WriteLine("Print Done"); return true;
-        }
-        public bool ScanContent(string content)
-        {
-            Console.WriteLine("Scan Done"); return true;
-        }
-        public bool PrintDuplexContent(string content)
+    }
+
+    public class CannonMG2470 : MultifunctionalPrinter
+    {
+        public override bool PrintDuplexContent(string content)
         {
             return false;
         }
-        public bool FaxContent(string content)
+        public override bool FaxContent(string content)
         {
             return false;
         }
